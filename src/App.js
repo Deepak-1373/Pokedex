5068462d063c5f73cc3060f9d97c0f1a01e27a4e
Deepak Kamat <dkamat.001@gmail.com>
2022-02-09 19:42:44 +0530

    feat: add state to store the data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Pokedex from "./components/pokedex/pokedex";
+import { pokemonData } from "./data/pokemonData";
 
 const App = () => {
+  const [appState, setState] = useState([
+    {
+      searchField: "",
+      allPokemons: [],
+      searchedPokemons: [],
+      selectedPokemon: undefined,
+    },
+  ]);
+
+  const [play, setPlay] = useState([
+    {
+      name: "Madhav",
+    },
+  ]);
+
+  useEffect(() => {
+    const pokeData = pokemonData;
+    console.log(pokeData);
+  }, []);
   return (
     <div className="app">
       <h1>Pokedex</h1>
